ab9dd8f29bffbac9281c24f15777f0993206d0e2
celica Galindo <celica.galindo@gmail.com>
2022-05-25 05:48:32 +0200

    creation model, parcours et controleur utilisateur à finir

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+//import librairie Express
 const express = require("express");
+//import de Mongoose pour la base de donnée
 const mongoose = require("mongoose");
+//creation de l'application (express)
 const app = express();
+//import router
+const userRoutes = require("./routes/user");
 
 mongoose
   .connect(
@@ -11,7 +16,10 @@ mongoose
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
+  res.json({ message: "Votre requête a bien été reçue !" }); //objet retourné
 });
 
+app.use("/api/auth", userRoutes);
+
+//export de la const app pour y acceder depuis d'autres fichier
 module.exports = app;
---
+++ controllers/user.js
@@ -0,0 +1,63 @@
+const bcrypt = require("bcrypt");
+//const { use } = require("../app");
+
+const User = require("../models/User");
+
+const jwt = require('jsonwebtoken');
+
+//middlewar nouvel utilisateur
+//fonction pour enregistrer les nouveaux utilisateurs et crypter le mot de passe
+exports.signup = (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+
+      //enregistrement de l'utlisateur dans la base de donnée
+      use
+        .save()
+        .then(() => res.status(201).json({ message: "utlisateur crée" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+//middleware connection 
+//fonction pour connecter les utlisateurs existants
+exports.login = (req, res, next) => {
+  User.findOne({
+    email: req.body.email
+  })
+  .then(user => {
+    if (!user) {
+      return res.status(401).json({
+        error: 'Utilisateur non trouvé !'
+      });
+    }
+    //bcrypt compare les hashs(issue du meme string d'origine)
+    bcrypt.compare(req.body.password, user.password)
+    .then(valid => {
+      if (!valid) {
+        return res.status(401).json({
+          error: 'Mot de passe incorrect !'
+        });
+      }
+      res.status(200).json({ 
+        userId: user._id,
+        token: jwt.sign( 
+              {
+                userId: user._id
+              }, 'RANDOM_TOKEN_SECRET',{ expiresIn: '24h'})
+              
+            });
+          })
+          .catch(error => res.status(500).json({
+            error
+          }));
+      })
+      .catch(error => res.status(500).json({
+        error
+      }))};
